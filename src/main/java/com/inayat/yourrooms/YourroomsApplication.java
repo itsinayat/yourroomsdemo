4255c25bf7a5bfe3cf65ac3adf0d2b7813c5b0ec
Inayat Hussain <inayat>
2020-04-03 14:24:52 +0530

    first commit

---
+++ src/main/java/com/inayat/yourrooms/YourroomsApplication.java
@@ -0,0 +1,13 @@
+package com.inayat.yourrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourroomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourroomsApplication.class, args);
+	}
+
+}
